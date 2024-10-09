c42e9a0b6bfa7787a345d11c42eb101895e166c0
Guto <gutomourao05@gmail.com>
2024-10-09 16:36:27 -0300

    fixo a tela de loading e alterando destino email

--- php/send-email.php
+++ php/send-email.php
@@ -17,15 +17,15 @@
   ";
   
   //Emails para quem será enviado o formulário
-  $destino = "contato@developingsolutions.com.br";
-  $assunto = "Contato pelo Site";
+  $destino = "gutomourao05@gmail.com";
+  $assunto = "Contato pelo Site - Developing Solutions";
 
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
   $headers = "MIME-Version: 1.1\n";
   $headers .= "Content-type: text/html; charset=UTF-8\n";
   $headers .= "From: $destino\n"; // remetente
   $headers .= "Return-Path: $destino\n"; // return-path
-  $envio = mail("gutomourao05@gmail.com", $assunto, $arquivo, $headers);
+  $envio = mail($destino, $assunto, $arquivo, $headers);
  
   if($envio)
   echo "Mensagem enviada com sucesso";
